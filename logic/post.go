7a81114057b3b8a45a81dce330d256233e4043fc
jhq <2098128799@qq.com>
2024-07-31 12:26:27 +0800

    modify

--- logic/post.go
+++ logic/post.go
@@ -4,6 +4,7 @@ import (
 	"Web_app/dao/mysql"
 	"Web_app/models"
 	"Web_app/pkg/snowflake"
+	"go.uber.org/zap"
 )
 
 func CreatePost(p *models.Post) (err error) {
@@ -11,5 +12,34 @@ func CreatePost(p *models.Post) (err error) {
 	p.ID = snowflake.GenID()
 	// 2. 保存到数据库
 	return mysql.CreatePost(p)
-	// 3.
+	// 3. 返回
+}
+
+func GetPostById(pid int64) (data *models.ApiPostDetail, err error) {
+	// 查询并组合接口想用的数据
+	post, err := mysql.GetPostById(pid)
+	if err != nil {
+		zap.L().Error("mysql.GetPostById failed", zap.Int64("pif", pid), zap.Error(err))
+		return
+	}
+
+	// 根据作者id查询作者信息
+	user, err := mysql.GetUserById(post.AuthorID)
+	if err != nil {
+		zap.L().Error("mysql.GetUserById failed", zap.Int64("author_id", post.AuthorID), zap.Error(err))
+		return
+	}
+	// 根据社区ID查询社区详细信息
+	community, err := mysql.GetCommunityDetailById(post.CommunityID)
+	if err != nil {
+		zap.L().Error("mysql.GetCommunityDetailById failed", zap.Int64("community_id", post.CommunityID), zap.Error(err))
+		return
+	}
+
+	data = &models.ApiPostDetail{
+		AuthorName:      user.Username,
+		Post:            post,
+		CommunityDetail: community,
+	}
+	return
 }
